9540aaf25c01f7d466edcd2e3b0c7800eb392242
Danielle Gellis <danisyellis@gmail.com>
2018-05-20 09:54:56 -0700

    formats names and zipcodes, adds tests for those functions, adds watch to test script

--- parseCSV.js
+++ parseCSV.js
@@ -5,10 +5,6 @@ let csvData = "";
 
 //TODO:
 //tests
-//take in CSV from stdin
-//parse csv
-  //make sure addresses with commas in them are ok
-//entire CSV is in the UTF-8 character set
 //Timestamp column
   //should be formatted in ISO-8601 format
   //converted from pacific to US/Eastern
@@ -22,7 +18,6 @@ let csvData = "";
   //if that breaks the input, drop that row and send a message to stderr
 //send it out through stdout
 
-function toExport() {
   process.stdin.setEncoding(encoding);
   process.stdin.on('readable', () => {
     let chunk;
@@ -31,16 +26,45 @@ function toExport() {
     }
   })
   process.stdin.on('end', () => {
-    var datagrid = parser.parse(csvData);
-    console.log(datagrid.data);
+    var datagrid = parser.parse(csvData).data;
+    for(let i=1; i<datagrid.length; i++) {
+      currentRow = datagrid[i];
+      formatTimestamp(currentRow);
+      formatZipcode(currentRow);
+      formatNames(currentRow);
+      //console.log(row)
+    }
+  //either just log the whole thing, or loop through each row and send it to stdout...
     //process.stdout.write(csvData + "\n")
   });
-}
 
-toExport()
+  function formatTimestamp(row) {
+    //first column format with moment
+      //and make it est instead of pst
+    //console.log(row[0]);
+    //row[0]
+  }
 
+  function formatZipcode(row) {
+    let zipcode = row[2];
+    if(zipcode.length > 5) {
+      row[2] = zipcode.slice(0,5);
+    } else if (zipcode.length < 5) {
+      let toAdd = ""
+      for(let i=0; i < 5-zipcode.length; i++) {
+        toAdd += "0";
+      }
+      row[2] = toAdd + zipcode;
+    }
+  }
 
+  function formatNames(row) {
+    row[3] = row[3].toUpperCase()
+    console.log(row[3]);
+  }
 
 module.exports = {
-  toExport,
+  formatTimestamp,
+  formatZipcode,
+  formatNames
 }
--- tests.js
+++ tests.js
@@ -1,6 +1,56 @@
 const mocha = require('mocha');
 const expect = require('chai').expect;
 
-let funcsToTest = require("./parseCSV.js");
+let {formatTimestamp, formatZipcode, formatNames} = require("./parseCSV.js");
 
-funcsToTest.first();
+let datagrid = [[0,0,"12345678", "Dani Gellis"],[0,0,"123", "株式会社スタジオジ"]]
+
+describe("formatting zipcode", function() {
+  it("truncates a zip code if it's more than 5 digits", function() {
+    let row = datagrid[0];
+    formatZipcode(row)
+    expect(row[2]).to.equal("12345")
+  });
+  it("prepends zeros until the zipcode has 5 digits", function() {
+    let row = datagrid[1];
+    formatZipcode(row)
+    expect(row[2]).to.equal("00123")
+  });
+})
+
+describe("formatting names", function() {
+  it("converts all names to uppercase", function() {
+    let row = datagrid[0];
+    formatNames(row)
+    expect(row[3]).to.equal("DANI GELLIS")
+  });
+  it("handles names that are not the english alphabet", function() {
+    let row = datagrid[1];
+    // console.log("hi" + formatNames);
+    // expect(formatNames).to.not.throw()
+  });
+})
+
+
+
+/*
+describe("taking in and parsing a csv", function() {
+  it("can take in a csv file", function() {
+    before(function() {
+      //change stdout's default behavior so that I can test it
+      //https://github.com/mochajs/mocha/issues/1582
+      //*****https://stackoverflow.com/questions/18543047/mocha-monitor-application-output?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+      //or the two libraries in there!  https://www.npmjs.com/package/test-console or https://www.npmjs.com/package/intercept-stdout
+      funcsToTest.toExport()
+  });
+    expect(test.to.exist)
+  })
+})
+
+//
+//   describe("data is invalid", function() {
+//     it("prints a warning to stderr", function() {
+//
+//     })
+//   })
+*/
